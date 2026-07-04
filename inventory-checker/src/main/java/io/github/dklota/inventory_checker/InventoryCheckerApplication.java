02a5ed3bb7a112486f6d16336d45179a56591277
dklota <divleen2@gmail.com>
2026-07-03 17:21:44 -0700

    completed phase 2 - using springboot to extract data from json

---
+++ inventory-checker/src/main/java/io/github/dklota/inventory_checker/InventoryCheckerApplication.java
@@ -0,0 +1,12 @@
+package io.github.dklota.inventory_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryCheckerApplication.class, args);
+	}
+}
